0ab69b75b311f2c692c9ad728eb33d0d38e18abb
zhangqicheng <zhangqicheng@yunzhangfang.com>
2021-11-10 08:39:35 +0800

    feat(1):

--- index.ts
+++ index.ts
@@ -8,26 +8,30 @@
  *  V: value 值
  *  C: code 代码
  */
-const buildArrToObj = <L extends string, V extends number, C extends string>([
-  label,
-  value,
-  code,
-]: [L, V, C]): {
+
+type NodeType = [string, number, string];
+
+type buildArrToObj<L extends string, V extends number, C extends string> = {
   label: L;
   value: V;
   code: C;
-} => {
-  return { label, value, code };
 };
 
-const tsEnum1 = buildArrToObj(["未开始", 0, "UNDO"]);
-tsEnum1.label;
+// const tsEnum1 = buildArrToObj(["未开始", 0, "UNDO"]);
+// tsEnum1.label;
 
+type NodeType2 = ["label", "value", "code"];
+let arr: Array<any>;
 const traverseBuild = <L extends string, V extends number, C extends string>(
-  arr: Array<[string, number, string]>
-) => {
-  return arr.map((item) => buildArrToObj(item));
+  arr: [L, V, C][]
+): {
+  [i in NodeType2[number]]: buildArrToObj<L, V, C>[i];
+  // label: T[0];
+}[] => {
+  const res = arr.map((item) => buildArrToObj(item));
+  return res;
 };
+
 const tsEnum = traverseBuild([
   ["未开始", 0, "UNDO"],
   ["进行中", 1, "DOING"],
@@ -35,3 +39,30 @@ const tsEnum = traverseBuild([
 ]);
 
 tsEnum[0].label;
+
+type If<N, S, R> = (<T>() => T extends N ? 1 : 2) extends <T>() => T extends S
+  ? 1
+  : 2
+  ? R
+  : never;
+
+type GetReadonlyKeys<T> = Exclude<
+  keyof T,
+  {
+    [K in keyof T]: If<{ [F in K]: T[F] }, { -readonly [F in K]: T[F] }, K>;
+  }[keyof T]
+>;
+interface Todo {
+  readonly title: string;
+  description: string;
+  completed: boolean;
+}
+
+type Keys = GetReadonlyKeys<Todo>; // expected to be "title" | "description"
+
+const tuple = ["tesla", "model 3", "model X", "model Y"] as const;
+// your answers
+type TupleToObject<T extends readonly any[]> = {
+  [i in T[number]]: i;
+};
+let result: TupleToObject<typeof tuple>; // expected { tesla: 'tesla', 'model 3': 'model 3', 'model X': 'model X', 'model Y': 'model Y'}
