415098423807918df2a003bfa468b35cf01c548d
章启成 <910021538@qq.com>
2021-11-21 19:33:42 +0800

    feat(工具类型、):

--- index.ts
+++ index.ts
@@ -9,60 +9,58 @@
  *  C: code 代码
  */
 
-type NodeType = [string, number, string];
-
-type buildArrToObj<L extends string, V extends number, C extends string> = {
-  label: L;
-  value: V;
-  code: C;
-};
-
-// const tsEnum1 = buildArrToObj(["未开始", 0, "UNDO"]);
-// tsEnum1.label;
-
-type NodeType2 = ["label", "value", "code"];
-let arr: Array<any>;
-const traverseBuild = <L extends string, V extends number, C extends string>(
-  arr: [L, V, C][]
-): {
-  [i in NodeType2[number]]: buildArrToObj<L, V, C>[i];
-  // label: T[0];
-}[] => {
-  const res = arr.map((item) => buildArrToObj(item));
-  return res;
-};
-
-const tsEnum = traverseBuild([
+const originalOptions = [
   ["未开始", 0, "UNDO"],
   ["进行中", 1, "DOING"],
   ["已结束", 2, "DONE"],
-]);
+] as const;
 
-tsEnum[0].label;
+/**
+ * 泛型定义 :
+ * 入参: 数组
+ * 出参: 数组长度
+ */
+type LengthOfArray<T extends readonly any[]> = Loop<T>;
+type Loop<
+  S extends readonly any[],
+  Count extends any[] = []
+> = S extends readonly [infer A, ...infer B]
+  ? Loop<B, [...Count, 1]>
+  : Count["length"];
 
-type If<N, S, R> = (<T>() => T extends N ? 1 : 2) extends <T>() => T extends S
-  ? 1
-  : 2
-  ? R
-  : never;
+/**
+ * 泛型定义 :
+ * 入参：数组长度
+ * 返回值：0-n的数组
+ */
+type LengthToArr<
+  L extends number,
+  Arr extends number[] = []
+> = LengthOfArray<Arr> extends L
+  ? Arr
+  : LengthToArr<L, [...Arr, LengthOfArray<Arr>]>;
 
-type GetReadonlyKeys<T> = Exclude<
-  keyof T,
-  {
-    [K in keyof T]: If<{ [F in K]: T[F] }, { -readonly [F in K]: T[F] }, K>;
-  }[keyof T]
->;
-interface Todo {
-  readonly title: string;
-  description: string;
-  completed: boolean;
+function traverseBuild<T extends readonly any[], Index extends number = 0>(
+  arr: T
+): {
+  key: T;
+  length: LengthToArr<LengthOfArray<T>>;
+  // labels: UnwrapRef<T>;
+  labels: {
+    [K in keyof [1, 2, 3]]: ReadonlyArray<T[K]> extends ReadonlyArray<infer L>
+      ? L
+      : never;
+  };
+} {
+  return arr as any;
 }
 
-type Keys = GetReadonlyKeys<Todo>; // expected to be "title" | "description"
+const tsEnum = traverseBuild(originalOptions);
+const lab = tsEnum.labels;
 
-const tuple = ["tesla", "model 3", "model X", "model Y"] as const;
-// your answers
-type TupleToObject<T extends readonly any[]> = {
-  [i in T[number]]: i;
-};
-let result: TupleToObject<typeof tuple>; // expected { tesla: 'tesla', 'model 3': 'model 3', 'model X': 'model X', 'model Y': 'model Y'}
+// 递归地获取嵌套数据的类型
+// Recursively unwraps nested value bindings.
+// 如果是数组，循环解套
+export type UnwrapRef<T> = T extends Array<infer V> ? Array<UnwrapRef<V>> : T;
+const tsEnum1 = null as UnwrapRef<typeof originalOptions>;
+const lab1 = tsEnum1[1][2];
